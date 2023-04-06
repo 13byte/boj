954c70fcafb0935339708972d32330eb31be02f9
13byte <13bitadc@naver.com>
2023-04-06 15:50:19 +0900

    23.04.06_Greedy_Algorithm

--- greedy_algorithm/4796(캠핑)/4796.c
+++ greedy_algorithm/4796(캠핑)/4796.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
 
 int main(void)
 {
-	int L = 1, P = 1, V = 1;
+	int l, p, v, i = 1;
 
-	for (int i = 1;; i++)
-	{
-		int cnt = 0;
-		scanf("%d %d %d", &L, &P, &V);
-		if (L == 0 && P == 0 && V == 0)
-			break;
-
-		cnt += (V / P) * L;				  // Add L for every full period P
-		cnt += (V % P) < L ? (V % P) : L; // Add remaining days for the last period
-
-		printf("Case %d: %d\n", i, cnt);
-	}
+	while (scanf("%d %d %d", &l, &p, &v), l || p || v)
+		printf("Case %d: %d\n", i++, (v / p) * l + (v % p < l ? v % p : l));
 	return (0);
 }
