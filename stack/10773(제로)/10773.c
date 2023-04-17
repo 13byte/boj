e885479a5e92a090aa883315e82729e85790bad8
13byte <13bitadc@naver.com>
2023-04-17 20:54:42 +0900

    23.04.17_Array_Stack

---
+++ stack/10773(제로)/10773.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct s_node
+{
+	int data;
+} t_node;
+
+typedef struct s_stack
+{
+	int capacity;
+	int top;
+	t_node *nodes;
+} t_stack;
+
+t_stack *create_stack(int capacity);
+void push(t_stack *stack, int new_data);
+int pop(t_stack *stack);
+void destroy_stack(t_stack *stack);
+int is_empty(t_stack *stack);
+
+int main(void)
+{
+	int K;
+	int tmp;
+	int result = 0;
+
+	scanf("%d", &K);
+	t_stack *stack = create_stack(K);
+	for (int i = 0; i < K; ++i)
+	{
+		scanf("%d", &tmp);
+		if (tmp == 0)
+			pop(stack);
+		else
+			push(stack, tmp);
+	}
+	while (!(is_empty(stack)))
+		result += pop(stack);
+	printf("%d\n", result);
+	destroy_stack(stack);
+	return (0);
+}
+
+t_stack *create_stack(int capacity)
+{
+	t_stack *new_stack = (t_stack *)malloc(sizeof(t_stack));
+
+	new_stack->nodes = (t_node *)malloc(sizeof(t_node) * capacity);
+	new_stack->capacity = capacity;
+	new_stack->top = -1;
+	return (new_stack);
+}
+
+void push(t_stack *stack, int new_data)
+{
+	stack->nodes[++stack->top].data = new_data;
+}
+
+int pop(t_stack *stack)
+{
+	if (is_empty(stack))
+		return 0;
+	return (stack->nodes[stack->top--].data);
+}
+
+void destroy_stack(t_stack *stack)
+{
+	free(stack->nodes);
+	free(stack);
+}
+
+int is_empty(t_stack *stack)
+{
+	return (stack->top == -1);
+}
