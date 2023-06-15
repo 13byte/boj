2a10c3dfdd4e7f39e5fc1d66172fa1320f34623b
jeosong <13bitadc@naver.com>
2023-06-15 16:35:43 +0900

    23.06.15_N과_M_(6)

---
+++ Backtracking/15655/15655.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int dfs_arr[10];
+int arr[10];
+bool isused[10005];
+
+void dfs(int cur, int len)
+{
+	if (len == M)
+	{
+		for (int i = 0; i < M; ++i)
+			cout << dfs_arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = cur; i < N; ++i)
+	{
+		if (!(isused[arr[i]]))
+		{
+			dfs_arr[len] = arr[i];
+			isused[arr[i]] = true;
+			dfs(i, len + 1);
+			isused[arr[i]] = false;
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i)
+		cin >> arr[i];
+	sort(arr, arr + N);
+	dfs(0, 0);
+	return (0);
+}
